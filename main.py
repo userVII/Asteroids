36ef0a509aae5756fd4c5041672afe7c32612bd0
userVII <userVII@users.noreply.github.com>
2025-12-17 14:21:57 -0500

    Pygame FPS

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
-        log_state
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        dt = (1000 / clock.tick(60))
 if __name__ == "__main__":
     main()

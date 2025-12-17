3df4043d0af66887514198140be6bf2a5f9350f1
userVII <userVII@users.noreply.github.com>
2025-12-17 15:48:46 -0500

    Movement update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from player import Player
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen =w pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
